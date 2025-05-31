1866061a90007021b7f6197334bc797dcc94c71e
Marvin Nox <sinedtex@gmail.com>
2025-06-01 00:42:25 +0300

    scroll-fix

--- src/JS/menu.js
+++ src/JS/menu.js
@@ -1,18 +1,19 @@
 const menu = document.querySelector('[data-menu]');
 export const openBtn = document.querySelector('[data-menu-open]');
 export const closeBtn = document.querySelector('[data-menu-close]');
-
+export const menuLinks = document.querySelectorAll('[data-menu-link]');
 
 export function handleOpenMenu() {
     if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-        document.body.style.overflow = '';
+        handleCloseMenu();
     } else {
         menu.classList.add('active');
         document.body.style.overflow = 'hidden';
     }
 }
+
 export function handleCloseMenu() {
     menu.classList.remove('active');
     document.body.style.overflow = '';
 }
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { closeBtn, handleCloseMenu, handleOpenMenu, openBtn } from "./JS/menu";
+import { closeBtn, handleCloseMenu, handleOpenMenu, menuLinks, openBtn } from "./JS/menu";
 import { detectCookiePop, handleClosePopUp, popBtns } from "./JS/pop-up";
 
-openBtn.addEventListener('click', handleOpenMenu);
-closeBtn.addEventListener('click', handleCloseMenu)
+openBtn?.addEventListener('click', handleOpenMenu);
+closeBtn?.addEventListener('click', handleCloseMenu)
 popBtns.forEach(btn => {
     btn.addEventListener('click', handleClosePopUp);
 })
+menuLinks.forEach(link => {
+    link.addEventListener('click', handleCloseMenu);
+});
 
 detectCookiePop()
