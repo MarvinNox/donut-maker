ee0dbeb679100c62231f0bcfb4da6e38edb1339f
Marvin Nox <sinedtex@gmail.com>
2025-06-01 00:16:32 +0300

    fix-menu-styles+header-fixed

--- src/JS/menu.js
+++ src/JS/menu.js
@@ -4,8 +4,13 @@ export const closeBtn = document.querySelector('[data-menu-close]');
 
 
 export function handleOpenMenu() {
-    menu.classList.add('active');
-    document.body.style.overflow = 'hidden';
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    } else {
+        menu.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    }
 }
 export function handleCloseMenu() {
     menu.classList.remove('active');
