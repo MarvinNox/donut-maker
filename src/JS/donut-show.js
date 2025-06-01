b818660ef9e3b272714745aa1ccd584de72486f9
Marvin Nox <sinedtex@gmail.com>
2025-06-01 15:19:04 +0300

    hate-swiper

--- src/JS/donut-show.js
+++ src/JS/donut-show.js
@@ -5,13 +5,13 @@ export function initDonutShowcase() {
     new Swiper('.donut-showcase', {
         loop: true,
         centeredSlides: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 60,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        pagination: false,
-        scrollbar: false,
     });
 }
